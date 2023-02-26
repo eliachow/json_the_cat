0aa978b756dfe7f99c82975d57fad3cfd1a0c26f
eliachow <eliacchow@gmail.com>
2023-02-26 11:48:49 -0800

    input mocha chai testing, fix callback function to accept error and desc

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,19 +2,21 @@ const request = require('request');
 
 const fetchBreedDescription = function(breed, callback) {
 
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+  return request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
     if (error) {
-      console.error(error);
+      callback(error, null);
+      // console.error(error);
     }
 
     //JSON.parse translates the data into a readable object.
     const data = JSON.parse(body);
     
     //if the breed does not exist the result will be an empty array.
+
     if (data.length === 0) {
-      console.log("Breed not found 😿");
+      callback("Breed not found 😿", null);
     } else {
-      console.log(data[0]['description']);
+      callback(null, data[0]['description']);
     }
   });
 };
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ fetchBreedDescription(breed, (error, desc) => {
   if (error) {
     console.log('Error fetch details: ', error);
   } else {
-    console.log(desc);
+    console.log("Desc: ",desc);
   }
 });
 
